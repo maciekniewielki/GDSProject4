98d1ec8943bc586473ce3a92e97a547a4e44cbd7
szula27 <k.szulewa@gmail.com>
2016-09-14 20:36:58 +0200

    CarreerStats test

---
+++ Assets/Scripts/CareerStatistics.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class CareerStatistics : MonoBehaviour
+{
+    public Text playerShots;
+    public int ShotsSum;
+    private MatchStatistics statisticsToView;
+    
+    void Start ()
+    {
+        MatchStatistics stats = GameObject.Find("MatchStats").GetComponent<StatisticsManager>().endStatistics;
+        statisticsToView = stats;
+        ViewStatistics(stats);
+    }
+
+    public void ViewStatistics(MatchStatistics stats)
+    {
+        playerShots.text = "Shots: " + ShotsSum.ToString();
+        ShotsSum += stats.playerTeamShots;
+    }
+}
