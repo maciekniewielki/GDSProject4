34f856891c3a0a6f482a84c7d46e623e1f8cb3bc
Maciej Buczyński <kadlubken5@gmail.com>
2016-09-20 18:10:27 +0200

    Movies test

---
+++ Assets/Scripts/Not Used/Movie.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movie : MonoBehaviour
+{
+
+    public MovieTexture movie;
+	// Use this for initialization
+	void Start ()
+    {
+	    
+	}
+
+}
